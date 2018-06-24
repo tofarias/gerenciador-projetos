be4211be8ab0b72e00b5d38adb72f5e4ed4d28b1
Tiago Farias <tiago.farias.poa@gmail.com>
2018-06-24 10:03:26 -0300

    add tratando url

--- bootstrap.php
+++ bootstrap.php
@@ -2,4 +2,14 @@
 
 require __DIR__."/vendor/autoload.php";
 
-new \ToFarias\Framework\Router();
+$router = new \ToFarias\Framework\Router();
+
+$router->add('/', function(){
+    return 'Home';
+});
+
+$router->add('/projects/(\d+)', function($params){
+    return 'Projects '.$params[1];
+});
+
+echo $router->run();
--- src/Router.php
+++ src/Router.php
@@ -4,7 +4,44 @@ namespace ToFarias\Framework;
 
 class Router
 {
+    private $routes = [];
+
     public function __construct()
     {
+    }
+
+    public function add(string $pattern, $callback)
+    {
+        $pattern = '/^'.str_replace('/', '\/',$pattern).'$/';
+        $this->routes[$pattern] = $callback;
+    }
+
+    public function run()
+    {
+        //$url = $_SERVER['PATH_INFO'] ?? '/';
+        $url =$this->getUrl();
+
+        foreach ($this->routes as $route => $action)
+        {
+            if( preg_match($route, $url, $params) ){
+                return $action($params);
+            }
+        }
+
+        if( array_key_exists($route, $this->routes) ){
+            return $this->routes[$route]();
+        }
+        return 'Page Not Found';
+    }
+
+    public function getUrl()
+    {
+        $url = $_SERVER['PATH_INFO'] ?? '/';
+
+        if(strlen($url) > 1){
+            $url = rtrim($url, '/');
+        }
+
+        return $url;
     }
 }
